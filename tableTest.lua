165604c83825a7172dd745f9f06bc87f13ad8ec6
Aranoxx <aranoxx@hotmail.com>
2017-03-06 07:28:51 +0100

    Update tableTest.lua

--- tableTest.lua
+++ tableTest.lua
@@ -6,8 +6,8 @@ for a in pairs(mfsuLast) do   
   mfsuDiff[a] = mfsuPres[a] - mfsuLast[a]    
   if mfsuPres[a] >= 39.7 then mfsuChng[a] = 4    
   elseif mfsuPres[a] <= 0.5 then mfsuChng[a] = 1    
-  elseif mfsuPres[a] >= 0.5 and mfsuDiff[a] > 0 then mfsuChng[a] = 2
-  elseif mfsuPres[a] >= 0.5 and mfsuDiff[a] < 0 then mfsuChng[a] = 3
+  elseif mfsuPres[a] > 0.5 and mfsuDiff[a] > 0 then mfsuChng[a] = 2
+  elseif mfsuPres[a] > 0.5 and mfsuDiff[a] < 0 then mfsuChng[a] = 3
   end
 end
 for a in pairs(mfsuLast) do    print("mfsuLast = "..mfsuLast[a].."  // mfsuPres = "..mfsuPres[a].."  // mfsuDiff = "..mfsuDiff[a].."  // mfsuChng = "..mfsuChng[a]) end    
