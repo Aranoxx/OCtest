e3a71056e346d4f9604b29f6bbf00171dfc59ef0
Aranoxx <aranoxx@hotmail.com>
2017-03-06 07:27:37 +0100

    Update tableTest.lua

--- tableTest.lua
+++ tableTest.lua
@@ -1,1 +1,13 @@
-mfsuLast = {00,00,20,30}mfsuPres = {10,00,10,40}mfsuDiff = {}mfsuChng = {} -- 1 = empty, 2 = increase, 3 = decrease, 4 = fullfor a in pairs(mfsuLast) do    mfsuDiff[a] = mfsuPres[a] - mfsuLast[a]    if mfsuPres[a] >= 39.7 then mfsuChng[a] = 4    elseif mfsuPres[a] <= 0.5 then mfsuChng[a] = 1    elseif mfsuPres[a] >= 0.5 and mfsuDiff[a] > 0 then mfsuChng[a] = 2    elseif mfsuPres[a] >= 0.5 and mfsuDiff[a] < 0 then mfsuChng[a] = 3    endendfor a in pairs(mfsuLast) do    print("mfsuLast = "..mfsuLast[a].."  // mfsuPres = "..mfsuPres[a].."  // mfsuDiff = "..mfsuDiff[a].."  // mfsuChng = "..mfsuChng[a]) end    
+mfsuLast = {00,00,20,30}
+mfsuPres = {10,00,10,40}
+mfsuDiff = {}
+mfsuChng = {} -- 1 = empty, 2 = increase, 3 = decrease, 4 = full
+for a in pairs(mfsuLast) do    
+  mfsuDiff[a] = mfsuPres[a] - mfsuLast[a]    
+  if mfsuPres[a] >= 39.7 then mfsuChng[a] = 4    
+  elseif mfsuPres[a] <= 0.5 then mfsuChng[a] = 1    
+  elseif mfsuPres[a] >= 0.5 and mfsuDiff[a] > 0 then mfsuChng[a] = 2
+  elseif mfsuPres[a] >= 0.5 and mfsuDiff[a] < 0 then mfsuChng[a] = 3
+  end
+end
+for a in pairs(mfsuLast) do    print("mfsuLast = "..mfsuLast[a].."  // mfsuPres = "..mfsuPres[a].."  // mfsuDiff = "..mfsuDiff[a].."  // mfsuChng = "..mfsuChng[a]) end    
